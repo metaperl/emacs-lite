3a25064eccfe5da5ef91f8894263077fff229a31
Structure and Interpretation of Computer Programs <metaperl@gmail.com>
2020-01-07 13:58:44 -0800

    Update init.el

--- init.el
+++ init.el
@@ -3,6 +3,20 @@
 
 (setq dired-dwim-target t)
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+          'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
 
 (add-to-list 'load-path "~/emacs")
 
